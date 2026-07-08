f0d3bcdfb64d7fbb0ffc6cb59d2c25d9fc507038
hemanthkonakalla01 <hemanth.konakalla1996@gmail.com>
2026-07-08 10:52:10 +0530

    HemanthC1

--- BiologyTeacher.java
+++ BiologyTeacher.java
@@ -7,6 +7,7 @@ public class BiologyTeacher extends Teacher {
 	{
 		//sysout is used to print message on console
 		//Added new line by hemanthdev for new feature
+		//This is line no 10 added by hemanthdev
 		System.out.println("BiologyTeacher teaches biology subject");
 
 	}
